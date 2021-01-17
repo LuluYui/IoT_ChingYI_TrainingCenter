c2bcb2e2f37ec846f75b92d1c0bc56ddd08ead2a
LuluYui <yipchoyui1@gmail.com>
2021-01-18 03:45:32 +0800

    initializing next.js

---
+++ WebDev_PassionMusic/IoTFlowMeter/iot-web-portal/pages/analytics.js
@@ -0,0 +1,5 @@
+function Analytics() {
+    return <div> This page is to show the analytics </div>
+  }
+  
+  export default Analytics 
---
+++ WebDev_PassionMusic/IoTFlowMeter/iot-web-portal/pages/site_map.js
@@ -0,0 +1,5 @@
+function Site_Map() {
+    return <div>Site Map this page</div>
+  }
+  
+  export default Site_Map
