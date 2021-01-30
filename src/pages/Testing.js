3116536c5846eee011bb825de8577d2f24e7a9dc
LuluYui <yipchoyui1@gmail.com>
2021-01-30 13:21:08 +0800

    Initialize prototype

---
+++ src/pages/Testing.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+
+export default class Testing extends Component {
+  constructor(props) {
+    super(props);
+    this.chartRef = React.createRef();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return <h1>Hello World</h1>;
+    
+  }
+}
